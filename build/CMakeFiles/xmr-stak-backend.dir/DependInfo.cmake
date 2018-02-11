d0c13456b8fc4166d896cd363bac7824c601f43a
Your Name <you@example.com>
2018-02-11 01:53:54 +0000

    save

---
+++ build/CMakeFiles/xmr-stak-backend.dir/DependInfo.cmake
@@ -0,0 +1,46 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jiangzhongbo/xmr-dev/xmrstak/backend/backendConnector.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/backendConnector.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/backend/cpu/crypto/cryptonight_common.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/crypto/cryptonight_common.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/backend/cpu/hwlocMemory.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/hwlocMemory.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/backend/cpu/jconf.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/jconf.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/backend/cpu/minethd.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/cpu/minethd.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/backend/globalStates.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/backend/globalStates.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/http/httpd.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/http/httpd.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/http/webdesign.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/http/webdesign.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/jconf.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/jconf.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/misc/console.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/console.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/misc/executor.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/executor.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/misc/telemetry.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/telemetry.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/misc/uac.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/uac.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/misc/utility.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/misc/utility.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/net/jpsock.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/net/jpsock.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/net/socket.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/net/socket.cpp.o"
+  "/home/jiangzhongbo/xmr-dev/xmrstak/version.cpp" "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-backend.dir/xmrstak/version.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BACKEND_TYPE=cpu"
+  "CONF_NO_OPENCL"
+  "GIT_BRANCH=master"
+  "GIT_COMMIT_HASH=242e4f4"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jiangzhongbo/xmr-dev/build/CMakeFiles/xmr-stak-c.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
